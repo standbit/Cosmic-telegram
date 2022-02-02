8f68b32e6e4754c1673e53b6b033c6b1bbe5db4a
Stanislav <77130336+standbit@users.noreply.github.com>
2022-02-02 19:39:38 +0300

    Download several epic photos

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from urllib.parse import urlparse, unquote_plus
 from os.path import splitext
 from dotenv import load_dotenv
 from pprint import pprint
+from datetime import datetime
 
 
-def download_picture(url, filename):
+def download_image(url, filename):
     response = requests.get(url)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
 
 
-def get_nasa_image_link():
+def fetch_nasa_epic_images(img_num):
     url = "https://api.nasa.gov/EPIC/api/natural"
     token = os.getenv("NASA_TOKEN")
     payload = {
@@ -22,24 +23,35 @@ def get_nasa_image_link():
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    image_name = response.json()[0]["image"]
-    image_date = response.json()[0]["date"]
-    cutted_image_date = image_date[:10].replace("-", "/")
-    image_link = "https://epic.gsfc.nasa.gov/archive/natural/{date}/png/{img}.png".format(date=cutted_image_date, img=image_name)
-    return image_link
+    epic_json = response.json()
+    links = []
+    num = 0
+    while num < img_num:
+        image_name = epic_json[num]["image"]
+        image_date = epic_json[num]["date"]
+        cutted_image_date = datetime.fromisoformat(image_date).strftime("%Y/%m/%d")
+        image_link = "https://epic.gsfc.nasa.gov/archive/natural/{date}/png/{img}.png".format(date=cutted_image_date, img=image_name)
+        links.append(image_link)
+        num += 1
+    space_dir = "./nasa_epic_images/"
+    Path(space_dir).mkdir(parents=True, exist_ok=True)
+    for image_number, link in enumerate(links):
+            image_name = f"nasa_epic{image_number}.png"
+            filename = f"{space_dir}{image_name}"
+            download_image(link, filename)
 
 
-def fetch_spacex_launch(flight_number):
+def fetch_spacex_images(flight_num):
     space_dir = "./spacex_images/"
     Path(space_dir).mkdir(parents=True, exist_ok=True)
     url = "https://api.spacexdata.com/v3/launches/{flight_number}"
-    response = requests.get(url.format(flight_number=flight_number))
+    response = requests.get(url.format(flight_number=flight_num))
     response.raise_for_status()
     links = response.json()["links"]["flickr_images"]
     for image_number, link in enumerate(links):
             image_name = f"spacex{image_number}.jpg"
-            filename = space_dir + image_name
-            download_picture(link, filename)
+            filename = f"{space_dir}{image_name}"
+            download_image(link, filename)
 
 
 def get_file_extension(link):
@@ -48,14 +60,14 @@ def get_file_extension(link):
     return extension
 
 
-def fetch_nasa_pictures(number):
-    space_dir = "./nasa_images/"
+def fetch_nasa_apod_images(img_num):
+    space_dir = "./nasa_apod_images/"
     Path(space_dir).mkdir(parents=True, exist_ok=True)
     url = "https://api.nasa.gov/planetary/apod"
     token = os.getenv("NASA_TOKEN")
     payload = {
         "api_key": token,
-        "count": number
+        "count": img_num
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -68,16 +80,16 @@ def fetch_nasa_pictures(number):
     for image_number, link in enumerate(links):
         extension = get_file_extension(link)
         image_name = f"nasa{image_number}{extension}"
-        filename = space_dir + image_name
-        download_picture(link, filename)
+        filename = f"{space_dir}{image_name}"
+        download_image(link, filename)
 
 
 def main():
     load_dotenv()
     try:
-        # print(fetch_spacex_launch(30))
-        # fetch_nasa_pictures(30)
-        pprint(get_nasa_image_link())
+        fetch_nasa_epic_images(5)
+        fetch_nasa_apod_images(5)
+        fetch_spacex_images(16)
     except requests.exceptions.HTTPError as err:
             print("General Error, incorrect link\n", str(err))
     except requests.ConnectionError as err:
